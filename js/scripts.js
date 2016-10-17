e5f2b7c1c8fcd9ebc734005b5f4e2e32af3a7244
Chris Wingo and Nigel Burnett <chris@>
2016-10-17 10:57:36 -0700

    Add subtract, multiply, divide, remainder

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,25 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
+var sub = function(number1 , number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1 , number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1 , number2) {
+  return number1 / number2;
+};
+
+var remainder = function(number1 , number2) {
+  return number1 % number2;
+};
+
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 
-alert(add(number1, number2);
+
+var result = remainder(number1, number2);
+alert(result);
