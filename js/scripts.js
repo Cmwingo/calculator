69284fa92e7caa00e1995e76445f1b8e33803e2d
Wingo <cmwingo@gmail.com>
2016-10-20 21:28:51 -0700

    Give full functionality to the calculator with input taken from form

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
+$(document).ready(function() {
+  $("form#calculator").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = sub(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
+  });
+});
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -18,9 +37,9 @@ var remainder = function(number1 , number2) {
   return number1 % number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-
-
-var result = remainder(number1, number2);
-alert(result);
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+//
+//
+// var result = remainder(number1, number2);
+// alert(result);
